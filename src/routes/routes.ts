a742a65c8677ea1036a27ecfb8349b42719b654d
Samuel Deotti <samuel_deotti@hotmail.com>
2024-08-28 15:31:09 -0300

    adding confirm route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import MeasureController from '../controllers/measures.controller';
+import ConfirmController from '../controllers/confirm.controller';
 
 const measureController = new MeasureController();
+const confirmController = new ConfirmController();
 
 const router = express.Router();
 
 router.post('/upload', (req, res) => measureController.createMeasure(req, res));
+router.patch('/confirm', (req, res) => confirmController.confirmMeasure(req, res));
 
 export default router;
