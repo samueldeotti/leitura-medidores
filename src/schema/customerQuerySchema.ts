728a56739bc518aafc14ec479746cba439f4ed86
Samuel Deotti <samuel_deotti@hotmail.com>
2024-08-29 20:42:40 -0300

    clearing database

--- src/schema/customerQuerySchema.ts
+++ src/schema/customerQuerySchema.ts
@@ -5,7 +5,8 @@ export const querySchema = Joi.object({
     'string.empty': 'O parâmetro "customer_code" não pode estar vazio.',
     'string.uuid': 'O parâmetro "customer_code" deve ser um UUID válido.',
   }),
-  measure_type: Joi.string().valid('WATER', 'GAS').optional().uppercase().messages({
+  measure_type: Joi.string().valid('WATER', 'GAS').optional().uppercase()
+.messages({
     'any.only': 'Tipo de medição não permitida',
   }),
 });
--- src/services/measures.service.ts
+++ src/services/measures.service.ts
@@ -18,24 +18,23 @@ export default class MeasureService {
       const monthMeasureExists = userMeasures.find((userMeasure) => {
         return new Date(userMeasure.measureDatetime).getMonth() === new Date(measure.measureDatetime).getMonth() && userMeasure.measureType === measure.measureType;
       });
-      
-      if (monthMeasureExists) {
-        return { status: 'CONFLICT', data: { error_code: 'CONFIRMATION_DUPLICATE', error_description: 'Leitura do mês já realizada' } };
 
+      if (monthMeasureExists) {
+        return { status: 'CONFLICT', data: { error_code: 'DOUBLE_REPORT', error_description: 'Leitura do mês já realizada' } };
       }
 
       const { measureValue, imageUrl } = await getGeminiMeasure(measure.image);
-  
-      const createdMeasure = await this.measureModel.createMeasure({...measure, measureValue: Number(measureValue), image: imageUrl});
-  
+
+      const createdMeasure = await this.measureModel.createMeasure({ ...measure, measureValue: Number(measureValue), image: imageUrl });
+
       const formattedMeasure: ResponseMeasure = {
         image_url: createdMeasure.image,
         measure_value: createdMeasure.measureValue as number,
         measure_uuid: createdMeasure.uuid as number,
       }
-  
+
       return { status: 'SUCCESSFUL', data: formattedMeasure };
-      
+
     } catch (error) {
       return { status: 'INTERNAL_SERVER_ERROR', data: { error_code: 'INTERNAL_SERVER_ERROR', error_description: 'Internal server error' } };
     }
